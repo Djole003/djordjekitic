b37076603e580ec3913772f9a892243a1cd4335f
Djordje Kitic <djordjekitic2003@gmail.com>
2025-05-27 14:23:25 +0200

    Update start.bat with seeder run

--- start.bat
+++ start.bat
@@ -1,4 +1,6 @@
 @echo off
+cls
+
 echo Pokrecem composer install...
 composer install
 
@@ -6,10 +8,12 @@ echo Generisem app key...
 php artisan key:generate
 
 echo Pokrecem migracije...
-php artisan migrate
+php artisan migrate --force
+
+echo Pokrecem sve seedere...
+php artisan db:seed --force
 
 echo Pokrecem Laravel development server...
 php artisan serve --host=127.0.0.1 --port=8000
 
-echo Server je pokrenut. Pritisni bilo koji taster za kraj...
 pause
